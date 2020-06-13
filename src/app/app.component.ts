3ba1e7d9cd8c0bca02c503d8a45e94d4839df9dc
Nurridayanti P <nurridayanti.p@gmail.com>
2020-06-13 18:00:32 +0700

    added basic calc formula

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Calculator App';
+  num1: number;
+  num2: number;
+  result: number;
+  add() {
+      this.result = this.num1 + this.num2;
+  }
+  substract() {
+      this.result = this.num1 - this.num2;
+  }
+  multiply() {
+      this.result = this.num1 * this.num2;
+  }
+  divide() {
+      this.result = this.num1 % this.num2;
+  }
+}
